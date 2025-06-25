51e1a50f09109f21d2a82706ed6fb59d1c6ed0f3
Mimoalexer <MimoAlexer@gmail.com>
2025-06-25 11:26:02 +0200

    initial commit

---
+++ src/car/car_frame.rs
@@ -0,0 +1,17 @@
+struct CarFrame {
+    string: String,
+}
+
+impl CarFrame {
+    pub fn new() -> Self {
+        CarFrame {
+            string: String::from(
+                "o                       o
+                    |=======================|
+                    |  []   []   []   []    |
+                    |=======================|
+                    o                       o"
+            )
+        }
+    }
+}
